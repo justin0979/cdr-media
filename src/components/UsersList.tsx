7bdbc275b93368e325a6936e1d4db06882bc3a2f
justin0979 <justinm0979@gmail.com>
2023-04-05 12:05:20 -0500

    use useState() for checking whether users are being loaded

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,30 +1,43 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, fetchUsers, UsersState, addUser } from "../store";
 import { Button } from "./Button";
 import { Skeleton } from "./Skeleton";
 
 export function UsersList() {
-  const dispatch = useDispatch<AppDispatch>();
-  const { isLoading, data, error } = useSelector(
-    (state: { users: UsersState }) => {
-      return state.users;
-    },
+  // `useState()` is used here as an alternate option to using Redux Toolkit Query
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState<null | {}>(
+    null,
   );
+  const dispatch = useDispatch<AppDispatch>();
+  const { data } = useSelector((state: { users: UsersState }) => {
+    return state.users;
+  });
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingUsers(true);
+    /*
+     * `dispatch` breaks the promise's .then().catch() executing `.then()` regardless
+     * of a success or failure. To get around this, use `.unwrap()`.
+     * `.finally()` is used to remove rendundant code of adding:
+     *   `.then(() => setIsLoadingUsers(false)).catch((err) => isLoadingUsers(false))`
+     */
+    dispatch(fetchUsers())
+      .unwrap()
+      .catch((err) => setLoadingUsersError(err))
+      .finally(() => setIsLoadingUsers(false));
   }, []);
 
   const handleUserAdd = () => {
     dispatch(addUser());
   };
 
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Skeleton times={data.length} className="h-10 w-full" />;
   }
 
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   }
 
