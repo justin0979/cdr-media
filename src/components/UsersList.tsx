10adc4a8b57f920067820a86fe703e41be413efb
justin0979 <justinm0979@gmail.com>
2023-04-11 11:50:59 -0500

    move useThunk to own file

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,37 +1,9 @@
-import { useState, useEffect, useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, fetchUsers, UsersState, addUser } from "../store";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { fetchUsers, UsersState, addUser } from "../store";
 import { Button } from "./Button";
 import { Skeleton } from "./Skeleton";
-
-function useThunk(thunk: any) {
-  /*
-   * `useState()` is used here as an alternate option to using Redux Toolkit Query
-   */
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<null | {}>(null);
-  const dispatch = useDispatch();
-
-  const runThunk = useCallback(() => {
-    setIsLoading(true);
-    /*
-     * `dispatch` breaks the promise's .then().catch() executing `.then()` regardless
-     * of a success or failure. To get around this, use `.unwrap()`.
-     * `.finally()` is used to remove rendundant code of adding:
-     *   `.then(() => setIsLoadingUsers(false)).catch((err) => isLoadingUsers(false))`
-     */
-    dispatch(thunk())
-      .unwrap()
-      .catch((err: any) => setError(err))
-      .finally(() => setIsLoading(false));
-  }, [dispatch, thunk]);
-  /*
-   * without `as const`, will give error 'TS2349: This expression is not callable'
-   * see https://www.github.com/microsoft/TypeScript/issues/35423
-   * see https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#const-assertions
-   */
-  return [runThunk, isLoading, error] as const;
-}
+import { useThunk } from "../hooks";
 
 export function UsersList() {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
---
+++ src/hooks/use-thunk.ts
@@ -0,0 +1,31 @@
+import { useState, useCallback } from "react";
+import { useDispatch } from "react-redux";
+
+export function useThunk(thunk: any) {
+  /*
+   * `useState()` is used here as an alternate option to using Redux Toolkit Query
+   */
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<null | {}>(null);
+  const dispatch = useDispatch();
+
+  const runThunk = useCallback(() => {
+    setIsLoading(true);
+    /*
+     * `dispatch` breaks the promise's .then().catch() executing `.then()` regardless
+     * of a success or failure. To get around this, use `.unwrap()`.
+     * `.finally()` is used to remove rendundant code of adding:
+     *   `.then(() => setIsLoadingUsers(false)).catch((err) => isLoadingUsers(false))`
+     */
+    dispatch(thunk())
+      .unwrap()
+      .catch((err: any) => setError(err))
+      .finally(() => setIsLoading(false));
+  }, [dispatch, thunk]);
+  /*
+   * without `as const`, will give error 'TS2349: This expression is not callable'
+   * see https://www.github.com/microsoft/TypeScript/issues/35423
+   * see https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#const-assertions
+   */
+  return [runThunk, isLoading, error] as const;
+}
