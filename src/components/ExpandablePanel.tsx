611e9dd97d86242bb34375e8a7a37769801f24b2
justin0979 <justinm0979@gmail.com>
2023-04-14 12:44:28 -0500

    define and implement base expandable panel

---
+++ src/components/ExpandablePanel.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+
+interface ExpandablePanelProps {
+  header: ReactNode;
+  children: ReactNode;
+}
+
+export function ExpandablePanel({
+  header,
+  children,
+}: ExpandablePanelProps) {
+  return (
+    <div className="mb-2 border rounded">
+      <div className="flex p-2 justify-between items-center cursor-pointer">
+        <div className="flex flex-row items-center justify-between">
+          {header}
+        </div>
+      </div>
+      <div className="p-2 border-t">{children}</div>
+    </div>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export * from "./Panel";
 export * from "./UsersList";
 export * from "./Skeleton";
 export * from "./UsersListItem";
+export * from "./ExpandablePanel";
